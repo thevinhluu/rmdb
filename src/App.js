d015a5086de44cfa8244cb027519a5ba0ba09ca2
Luu The Vinh <luuthevinh1988@gmail.com>
2021-07-16 23:38:08 +0700

    Global Style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+// Styles
+import { GlobalStyle } from './GlobalStyle';
+
 function App() {
-  return (
-    <div className="App">
-      Start here.
-    </div>
-  );
+	return (
+		<div className='App'>
+			Start here.
+			<GlobalStyle />
+		</div>
+	);
 }
 
 export default App;
