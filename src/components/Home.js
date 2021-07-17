1df9977ace80ccc3216124617c2599ff45f4dbc3
Luu The Vinh <luuthevinh1988@gmail.com>
2021-07-17 18:07:22 +0700

    Fetch data from the API for Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+// API
+import API from '../API';
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
@@ -12,6 +14,30 @@ const Home = () => {
 	const [state, setState] = useState();
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(false);
+
+	const fetchMovies = async (page, searchTerm = '') => {
+		try {
+			setError(false);
+			setLoading(true);
+
+			const movies = await API.fetchMovies(searchTerm, page);
+
+			setState((prev) => ({
+				...movies,
+				results : page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+			}));
+		} catch (error) {
+			setError(true);
+		}
+		setLoading(false);
+	};
+
+	// Initial render
+	useEffect(() => {
+		fetchMovies(1);
+	}, []);
+
+	console.log(state);
 
 	return <div>Home Page</div>;
 };
