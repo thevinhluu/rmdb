6a158ea8502c591b1cc5daa830ef0b22d480bc1a
Luu The Vinh <luuthevinh1988@gmail.com>
2021-07-18 10:35:07 +0700

    Thumb Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
 import HeroImage from './HeroImage';
 import Grid from './Grid';
+import Thumb from './Thumb';
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -24,7 +25,16 @@ const Home = () => {
 					text={state.results[0].overview}
 				/>
 			) : null}
-			<Grid header='Popular Movies'>{state.results.map((movie) => <div key={movie.id}>{movie.title}</div>)}</Grid>
+			<Grid header='Popular Movies'>
+				{state.results.map((movie) => (
+					<Thumb
+						key={movie.id}
+						clickable
+						image={movie.poster_path ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path : NoImage}
+						movieId={movie.id}
+					/>
+				))}
+			</Grid>
 		</React.Fragment>
 	);
 };
