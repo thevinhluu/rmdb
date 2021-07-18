4c8569f2e8402d108f549ad99f312552fc668977
Luu The Vinh <luuthevinh1988@gmail.com>
2021-07-18 17:33:52 +0700

    Button Component - Logic

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // Styles
-import { Wrapper } from '../HeroImage/HeroImage.styles';
+import { Wrapper } from './Button.styles';
 
 const Button = ({ text, callback }) => (
 	<Wrapper type='button' onClick={callback}>
