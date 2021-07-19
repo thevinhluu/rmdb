9550f7f1a94b33d54a0fe7c15b4e17cab53e3564
Luu The Vinh <luuthevinh1988@gmail.com>
2021-07-19 12:33:46 +0700

    Movie Component - Scaffold

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,20 @@
 import React from 'react';
+// Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+// Components
+import Grid from './Grid';
+import Spinner from './Spinner';
+// Hook
 
-const Movie = () => <div>Movie</div>;
+// Image
+import NoImage from '../images/no_image.jpg';
+
+const Movie = () => {
+	return (
+		<React.Fragment>
+			<div>Movie</div>
+		</React.Fragment>
+	);
+};
 
 export default Movie;
