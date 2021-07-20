5a491008a739a14f8f8ee62b84e03d9d8ed86017
Luu The Vinh <luuthevinh1988@gmail.com>
2021-07-20 16:37:45 +0700

    MovieInfo Component - Styles

--- src/components/MovieInfo/index.js
+++ src/components/MovieInfo/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 // Components
 import Thumb from '../Thumb';
 // Config
@@ -14,26 +15,29 @@ const MovieInfo = ({ movie }) => (
 			<Thumb
 				image={movie.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}` : NoImage}
 				clickable={false}
-				alt='movie-thumb'
 			/>
-		</Content>
-		<Text>
-			<h1>{movie.title}</h1>
-			<h3>PLOT</h3>
-			<p>{movie.overview}</p>
+			<Text>
+				<h1>{movie.title}</h1>
+				<h3>PLOT</h3>
+				<p>{movie.overview}</p>
 
-			<div className='rating-directors'>
-				<div>
-					<h3>RATING</h3>
-					<div className='score'>{movie.vote_average}</div>
+				<div className='rating-directors'>
+					<div>
+						<h3>RATING</h3>
+						<div className='score'>{movie.vote_average}</div>
+					</div>
+					<div className='director'>
+						<h3>DIRECTOR{movie.directors.length > 1 ? 'S' : ''}</h3>
+						{movie.directors.map((director) => <p key={director.credit_id}>{director.name}</p>)}
+					</div>
 				</div>
-				<div className='director'>
-					<h3>DIRECTOR{movie.directors.length > 1 ? 'S' : ''}</h3>
-					{movie.directors.map((director) => <p key={director.credit_id}>{director.name}</p>)}
-				</div>
-			</div>
-		</Text>
+			</Text>
+		</Content>
 	</Wrapper>
 );
+
+MovieInfo.propTypes = {
+	movie : PropTypes.object
+};
 
 export default MovieInfo;
